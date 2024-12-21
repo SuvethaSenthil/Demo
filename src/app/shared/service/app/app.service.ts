140e8427dbdbf3fcc767b5ac4b768a4ea067fe1f
Suvetha S <161910830+SuvethaSenthil@users.noreply.github.com>
2024-12-21 22:27:36 +0530

    Update app.service.ts

--- src/app/shared/service/app/app.service.ts
+++ src/app/shared/service/app/app.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { TokenService } from '../token/token.service';
+import { LoginResponse } from '../../model/login.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  private api_url = 'http://localhost:5000';
+  private api_url = 'http://localhost:3000';
+  private tokenService = inject(TokenService);
 
-  constructor(private http: HttpClient, private tokenService: TokenService) { }
+  constructor(private http: HttpClient, ) { }
 
   register(body: UserData) {
     const url = this.api_url + '/api/auth/register';
@@ -21,24 +23,33 @@ export class AppService {
     // const headers = new HttpHeaders({
     //   Authorization: `Bearer ${this.tokenService.getToken()}`,
     // });
-    return this.http.post/*<LoginResponse>*/(url, body); // this.http.post<LoginResponse>(url,body,{headers});
+    return this.http.post<LoginResponse>(url, body); // this.http.post<LoginResponse>(url,body,{headers});
   }
 
   /*constructor(private http:HttpClient) { }*/
 
   generateplan(data: any) {
-    const url = "http://localhost:5000/plan/generate-plan";
+    const url = "http://localhost:3000/plan/generate-plan";
+    console.log(this.tokenService.getToken())
     const headers = new HttpHeaders({
       Authorization: `Bearer ${this.tokenService.getToken()}`,
     });
     return this.http.post(url, data, {headers});
   }
   validateQuiz(data: any) {
-    const url = "http://localhost:5000"+"add the quiz API path here"; //I'm not sure to to call which API endpoint.
+    const url = "http://localhost:3000"+"add the quiz API path here"; //I'm not sure to to call which API endpoint.
     const headers = new HttpHeaders({
       Authorization: `Bearer ${this.tokenService.getToken()}`,
     });
     return this.http.post(url, data, {headers}); // check API, whether you need to use POST or PUT method...
+  }
+
+  generateQuiz(body:any){
+    const url = this.api_url+'/study/quiz';
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.tokenService.getToken()}`,
+    });
+    return this.http.post(url,body, {headers});
   }
 }
 
