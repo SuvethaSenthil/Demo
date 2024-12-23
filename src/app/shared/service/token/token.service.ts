ad6e5a24dc0fbb72711d3098eba98eadce2ccd5a
Suvetha S <161910830+SuvethaSenthil@users.noreply.github.com>
2024-12-23 23:12:46 +0530

    Update token.service.ts

--- src/app/shared/service/token/token.service.ts
+++ src/app/shared/service/token/token.service.ts
@@ -1,20 +1,23 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
-  private token = signal<string>('');
+  private token =new BehaviorSubject<string>('');
 
   constructor() { }
 
   setToken(token: string) {
-    this.token.set(token);
+    this.token.next(token);
+    console.log("Token has been updated");
+    localStorage.setItem('token',token);
   }
 
   getToken() {
-    return this.token();
+    return localStorage.getItem('token');
   }
 
 }
